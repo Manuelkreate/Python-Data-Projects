200d0c5475d181b9ef9aed3dd8b2a57d92e0e8af
Manuelkreate <110608955+Manuelkreate@users.noreply.github.com>
2025-05-20 18:55:21 +0100

    Update feauture_engineering.py
    Forgot to save the entire thing, so I completed it.

--- feauture_engineering.py
+++ feauture_engineering.py
@@ -3,5 +3,11 @@ nutrition_df = pd.read_csv('Nutrition.csv')
 print('Original Data: ')
 print(nutrition_df)
 
-df['IsHealthy'] = df['Calories']<200
-df['PerBite'] = df['Calories']/5
+nutrition_df['IsHealthy'] = nutrition_df['Calories']<200
+nutrition_df['PerBite'] = nutrition_df['Calories']/5
+category_map = {'Snack':0, 'Dairy':1, 'Breakfast':2, 'Drink':3, 'Fruit':4, 'Lunch':5}
+nutrition_df['CategoryCode'] = nutrition_df['Category'].map(category_map)
+print('\nEngineered Data;')
+print(nutrition_df)
+
+nutrition_df.to_csv('Nutrition_Featured.csv', index=False)
